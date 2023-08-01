ef5bf7716de7fce1fb150eae876dd535d2830780
Adam Bednarz <adam.bednarz@infinite.pl>
2023-08-01 16:43:27 +0200

    added spring project

---
+++ src/main/java/com/example/xcodesoftwarerecruitment/XcodeSoftwareRecruitmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.xcodesoftwarerecruitment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XcodeSoftwareRecruitmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XcodeSoftwareRecruitmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/xcodesoftwarerecruitment/XcodeSoftwareRecruitmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xcodesoftwarerecruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XcodeSoftwareRecruitmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
